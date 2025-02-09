ecbfc2a7025564c461139eac4aada719b4a7e083
Akash OG <134829340+Akashog123@users.noreply.github.com>
2025-02-09 14:43:45 +0000

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import path from "path";
 
 export default defineConfig({
-  base: './', 
-  plugins: [
-    react()
-  ],
+  base: "./",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
+    },
   },
   server: {
+    host: "0.0.0.0",
+    allowedHosts: ["pns2zy-5173.csb.app"],
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
